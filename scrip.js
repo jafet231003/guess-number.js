00e8ebbdf9cbddb535799f4e993435f8986712b1
jafet231003 <jafetsanchez683@gmail.com>
2026-08-23 21:20:52 -0500

    Primer commit

---
+++ scrip.js
@@ -0,0 +1,20 @@
+console.log("Bienvendios a la clase de fundametos de programacion en enter tech!");
+
+let nombre = "jafet sanchez";
+console.log(nombre);
+console.log(typeof nombre);
+
+let edad = 22;
+console.log(edad);
+console.log(typeof edad);
+
+let nombreususario=prompt("¿Como te llamas ?");
+
+console.log("nombre" ,nombreususario);
+console.log("Tipo de dato", typeof nombreususario);
+
+
+let edadususario=prompt("¿Cuantos años tienes ?");
+
+console.log("nombre" ,edadususario);
+console.log("Tipo de dato", typeof edadususario);
