638c38915f77ab8b7f4abc12fc5d40f7b98f7418
jafet231003 <jafetsanchez683@gmail.com>
2026-08-30 20:49:41 -0500

    actualizacion del juego adivina el numero

--- scrip.js
+++ scrip.js
@@ -1,38 +1,4 @@
-/*console.log("Bienvendios a la clase de fundametos de programacion en enter tech!");
-
-let nombre = "jafet sanchez";
-console.log(nombre);
-console.log(typeof nombre);
-
-let edad = 22;
-console.log(edad);
-console.log(typeof edad);
-
-let nombreususario=prompt("¿Como te llamas ?");
-
-console.log("nombre" ,nombreususario);
-console.log("Tipo de dato", typeof nombreususario);
-
-
-let edadususario=prompt("¿Cuantos años tienes ?");
-
-console.log("nombre" ,edadususario);
-console.log("Tipo de dato", typeof edadususario);*/
-
-let edad = Number(prompt("¿Cuántos años tienes?"));
-
-if (edad >= 18) {
-    alert("Puedes votar 🗳️");
-} else {
-    alert("Aún no puedes votar ❌");
-}
-
-let temperatura = Number(prompt("¿Qué temperatura hace hoy?"));
-
-if (temperatura < 15) {
-    alert("Hace frío, ponte abrigo 🧥");
-} else if (temperatura <= 23) {
-    alert("Está fresquito, ponte algo ligero 🧢");
-} else {
-    alert("Hace calor, usa polo y bloqueador solar 🧴");
-}
+const contador = document.getElementById("contador");
+const mensaje =document.getElementById("mensaje");
+console.log(contador);
+console.log(mensaje);
